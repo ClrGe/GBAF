35d23ddb89f504fd383816df4327237e8bbf6e93
Clergé <65242993+Lokda-spec@users.noreply.github.com>
2021-05-07 11:28:51 +0200

    Update commenter.php

--- partenaires/commenter.php
+++ partenaires/commenter.php
@@ -10,15 +10,29 @@
       <a href="index.php"><img src="../img/logo.png" alt="GBAF"></a>
       <h1>Poster un commentaire</h1>
     </header>
-       <?php 
-            include("database.php");
-      ?>
+      
     <main>
       <form action="" method="post">
+        <label for="nom">Nom</label> : <input type="text" name="nom" id="nom" /><br />
         <label for="commentaire"></label>
         <textarea id="commentaire" rows="3" placeholder="Écrivez ici votre commentaire..." name="commentaire" required></textarea>
         <input type="submit" name="publier" value="Publier">
       </form>
+    
+      <p><strong><?php echo htmlspecialchars($_POST['nom']) ?></strong> le <?php echo date("d/m/Y") ?> à <?php echo date("H:i:s"); ?></p>
+      <p><?php echo nl2br(htmlspecialchars($_POST['commentaire'])); ?></p>
+    
+     <?php 
+          include("database.php");
+          $req = $bdd->prepare('INSERT INTO commentaires(id, id_user, id_partenaires, date_com, commentaires) VALUES(:id, :nom, :id_partenaires, :date_com, :commentaire)');
+          $req->execute(array(
+          'id' => $_POST[''],
+          'id_billet' => $_POST[''],
+          'nom' => $_POST['nom'],
+          'commentaire' => $_POST['commentaires'],
+          'date_com' => $NOW()
+               ));
+      ?>
     </main>
     <footer>
       <a>Mentions légales</a>
