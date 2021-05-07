34dad521a9718550b60bd5f499a43a74eaa3251f
Clergé <65242993+Lokda-spec@users.noreply.github.com>
2021-05-07 10:20:47 +0200

    Update extranetGBAF.sql

--- extranetGBAF.sql
+++ extranetGBAF.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 07, 2021 at 01:05 AM
+-- Generation Time: May 07, 2021 at 10:19 AM
 -- Server version: 8.0.23-0ubuntu0.20.04.1
 -- PHP Version: 7.4.3
 
@@ -36,6 +36,18 @@ CREATE TABLE `commentaires` (
   `commentaires` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `commentaires`
+--
+
+INSERT INTO `commentaires` (`id`, `id_user`, `id_partenaires`, `date_com`, `commentaires`) VALUES
+(1, 1, 1, '2021-05-02 14:31:02', 'Excellent service !'),
+(2, 2, 1, '2021-05-02 18:12:58', 'Très beau projet pour rendre accessible la formation professionnelle'),
+(3, 1, 4, '2021-05-03 08:41:02', 'Jamais déçu de ce service'),
+(4, 3, 2, '2021-05-03 10:39:11', 'Très intéressant'),
+(5, 3, 1, '2021-05-03 10:42:09', 'Toujours de bons retours de cette organisme'),
+(6, 4, 1, '2021-05-03 14:01:47', 'Un très beau projet porté par des gens efficaces');
+
 -- --------------------------------------------------------
 
 --
@@ -46,8 +58,18 @@ CREATE TABLE `partenaires` (
   `id` int NOT NULL,
   `nom` varchar(255) NOT NULL,
   `description` text NOT NULL,
-  `logo` varchar(255) NOT NULL
+  `logo` varbinary(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `partenaires`
+--
+
+INSERT INTO `partenaires` (`id`, `nom`, `description`, `logo`) VALUES
+(1, 'Formation&Co', 'Formation&co est une association française présente sur tout le territoire. <br>\n        Nous proposons à des personnes issues de tout milieu de devenir entrepreneur grâce\n        à un crédit et un accompagnement professionnel et personnalisé.', 0x466f726d26436f),
+(2, 'ProtectPeople', 'Chez Protectpeople, chacun cotise selon ses moyens et reçoit selon ses besoins. ProtectPeople est ouvert à tous.', 0x50726f7465637450656f706c65),
+(3, 'DSA France', 'Dsa France accélère la croissance du territoire et s’engage avec les collectivités territoriales.', ''),
+(4, 'CDE', 'La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans leurs démarches de formation. \nSon président est élu pour 3 ans par ses pairs, chefs d’entreprises et présidents des CDE.', '');
 
 -- --------------------------------------------------------
 
@@ -95,13 +117,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `commentaires`
 --
 ALTER TABLE `commentaires`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `partenaires`
 --
 ALTER TABLE `partenaires`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user`
