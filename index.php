a81452ef2ecd7a9b64d19fd9ff5f3793742881e3
Clergé <65242993+Lokda-spec@users.noreply.github.com>
2021-05-07 13:22:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <!-- page d'accueil de la plateforme -->
 <!DOCTYPE html>
 <html lang="fr">
-
+<head>
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>Accueil</title>
+   <link rel="stylesheet" href="css/style.css">
+</head>
 <?php 
 include("header.php");
 include("gbaf.php");
