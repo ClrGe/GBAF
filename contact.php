d8b4494645dc2aa5ec9feaddc2b719620e5cb684
Clergé <65242993+Lokda-spec@users.noreply.github.com>
2021-05-07 14:21:09 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -6,6 +6,5 @@
     <h3>Nous contacter</h3>
     <p>Numéro de téléphone : +33.00.00.00.00</p>
     <p>Adresse postale : 666, avenue Blip<br>00000 FRANCE</p>
-  </body>
   <?php include("footer.php");?>
 </html>
