306c7330028e02a94717f45303185160a216843b
Clergé <65242993+Lokda-spec@users.noreply.github.com>
2021-06-14 13:21:35 +0200

    Update partenaires.php

--- partenaires.php
+++ partenaires.php
@@ -1,68 +1,68 @@
 <?php
-  if (session_status() == PHP_SESSION_NONE) {
+  if (session_status() == PHP_SESSION_NONE) { // paramètres de session
   session_start();
   }
-  // verifier la connexion du visiteur ; sinon renvoi vers la page de connexion
   if (!isset($_SESSION['id'])) {
-  header('Location: connexion.php');
+  header('Location: connexion.php'); // verifier la connexion ; sinon renvoi vers la page d'identification
   die();
   }
-  // se connecter à la bdd
+//récupérer les infos du partenaires dans la bdd
   require "templates/database.php";
-  if (isset($_GET['id_partenaires'])){
-      $id_partenaires = $_GET['id_partenaires'];
-      $req->execute(array(
-      'id_partenaires' => $id_partenaires));
-      $result = $req->fetch();
-    }
+  if (isset($_GET['id'])){
+    $id_partenaires = $_GET['id'];
+    $req = $bdd->prepare('SELECT id, nom, description, logo FROM partenaires WHERE id = :id_partenaires'); // récupérer le partenaire
+    $req->execute(array(
+    'id_partenaires' => $id_partenaires));
+  }
+$req->execute(array($_GET['partenaires']));
+//préparer l'espace commentaires
+// vérification variables $_POST
+if (isset($_POST['id_user']) && isset($_POST['id_partenaires']) && isset($_POST['commentaires'])) {
+    $id_user = $_POST['id_user'];
+    $id_partenaires = $_POST['id_partenaires'];
+	$commentaires = htmlspecialchars($_POST['commentaires']);
+	// verser dans la bdd
+	$req = $bdd->prepare('INSERT INTO commentaires(id_user, id_partenaires, date_com, commentaires) VALUES (?, ?, NOW(), ?)');
+	$req->execute(array(
+		$id_user => htmlspecialchars($_POST['id_user']),
+		$id_partenaires = htmlspecialchars($_POST['id_partenaires']),
+        $commentaires = htmlspecialchars($_POST['commentaires'])));
+        $req->closeCursor();
+        header('Location: partenaires.php?id=' . $id_partenaires);
+}
+
+  //afficher les templates
+  require "templates/head.php";
+  require "templates/header.php";
 ?>
-    <?php require "templates/head.php";
-      require "templates/header.php"; ?>
-    <section class="partner-section bg-red">
+  <div class="partner-section bg-red">
+    <?php while ($result = $req->fetch()){
+      // afficher les données du partenaire
+        echo '<div><img src="img/partenaires/' . htmlspecialchars($result['logo']) . '" alt="Logo ' . htmlspecialchars($result['nom']) . '" class="pub"/></div>';
+        echo '<p class="large-txt white bold center">' . htmlspecialchars($result['description']) . '</p>';
+    }?>
+  </div>
+  <div class="comment-section"><hr>
+    <div class="flex-container">
+      <?php echo '<div class="button red"><i class="fas fa-thumbs-up"></i><a href="votes.php?votes=like&id=' . $id_partenaires . '" class="button"> J\'AIME</a></div>';
+      echo '<div class="button red"><i class="fas fa-thumbs-down"></i><a href="partenaires.php?id=' . $id_partenaires . '" class="button"> JE N\'AIME PAS</a></div>';?>
+    </div><hr>
+    <h2 class="comment-title big center red"><i class="fas fa-comments"></i> COMMENTAIRES </h2>
+    <div class="post-comment">
+        <?php echo '<form action="partenaires.php?id=' . $id_partenaires ;'" method="post"> ';?>
+        <input type="hidden" name="id_partenaires" value="<?php echo $id_partenaires; ?>"/>
+        <input type="hidden" name="id_user" value="<?php echo $_SESSION['id_user'] ?>"/>
+        <label for="commentaires"></label><br />
+        <br /><textarea id="commentaires" rows="3" class="commArea" placeholder="Écrivez ici votre commentaire..." name="commentaires" required></textarea>
+        <input type="submit" id='submit' name="publier" value="Publier" class="bg-red white publish">
+      </form>
+    </div><hr>
     <?php
-         $id = $_GET['id'];
-         $req = $bdd->prepare('SELECT id, nom, description, logo FROM partenaires WHERE id = :id');
-         $req->execute(array(
-         'id' => $id
-         ));
-         while ($donnees = $req->fetch()){
-		echo '<div><img src="img/partenaires/' . htmlspecialchars($donnees['logo']) . '" alt="Logo ' . htmlspecialchars($donnees['nom']) . '" class="pub"/></div>';
-		echo '<p class="large-txt white bold center">' . htmlspecialchars($donnees['description']) . '</p>';};
-	?>
-    </section>
-    <section class="comment-section">
-    <hr>
-    <?php
-		 $likes = $bdd->prepare("SELECT * FROM votes WHERE votes='1' AND id_partenaires = ?");
-     $likes -> execute(array($id));
-     $likes = $likes-> rowCount();
-     
-     $dislikes = $bdd->prepare("SELECT * FROM votes WHERE votes='2' AND id_partenaires = ?");
-     $dislikes -> execute(array($id));
-     $dislikes = $dislikes -> rowCount();
-	?>
-        <div class="flex-container">
-          <div class="button red"><i class="fas fa-thumbs-up"></i><a href="votes.php" class="button"> J'AIME</a></div>
-          <div class="button red"><i class="fas fa-thumbs-down"></i><a href="votes.php" class="button"> JE N'AIME PAS</a></div>
-        </div><hr>
-        <h2 class="comment-title big center red"><i class="fas fa-comments"></i> COMMENTAIRES </h2>
-        <div class="post-comment">
-          <form action="partenaires.php" method="post">
-            <input type="hidden" name="id_partenaires" value="<?php echo $id_partenaires; ?>"/>
-            <input type="hidden" name="id_user" value="<?php echo $_SESSION['id_user'] ?>"/>
-            <label for="commentaires"></label><br />
-            <br /><textarea id="commentaires" rows="3" class="commArea" placeholder="Écrivez ici votre commentaire..." name="commentaires" required></textarea>
-            <input type="submit" name="publier" value="Publier" class="bg-red white publish">
-          </form>
-        </div><hr>
-        <?php
-            $req = $bdd->prepare('SELECT id_user, commentaires, DATE_FORMAT(date_com, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire_fr  FROM commentaires WHERE id_partenaires = ? ORDER BY date_com');
-            $req->execute(array($id_partenaires));
-              while ($donnees = $req->fetch()){ ?>
-                <p class="comment black comment-info"><strong><?php echo htmlspecialchars($donnees['username']); ?></strong> le <?php echo $donnees['date_commentaire_fr']; ?></p>
-                <p class="comment black"><?php echo nl2br(htmlspecialchars($donnees['commentaires'])); ?></p><hr>
-        <?php }
-          $req->closeCursor();
-        ?>
-      </section>
-    <?php require "templates/footer.php";?>
+      $req_com = $bdd->prepare('SELECT id_user, commentaires, DATE_FORMAT(date_com, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire_fr  FROM commentaires WHERE id_partenaires = ? ORDER BY date_com');
+      $req_com->execute(array($id_partenaires));
+      while ($donnees = $req_com->fetch()){ ?>
+        <p class="comment black comment-info"><strong><?php echo htmlspecialchars($donnees['username']); ?></strong> le <?php echo $donnees['date_commentaire_fr']; ?></p>
+        <p class="comment black"><?php echo nl2br(htmlspecialchars($donnees['commentaires'])); ?></p><hr>
+        <?php } $req_com->closeCursor();?>
+  </div>
+  <?php require "templates/footer.php";?>
