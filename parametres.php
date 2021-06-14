29e53091ba62fb203c72e45b9b03fb3e04ddc490
Clergé <65242993+Lokda-spec@users.noreply.github.com>
2021-06-14 21:51:49 +0200

    Update parametres.php

--- parametres.php
+++ parametres.php
@@ -7,40 +7,34 @@
     header('Location: connexion.php');
     die();
   }
-  require "templates/database.php";
-?>
-<?php
-    if (isset($_POST['id']) && isset($_POST['username']) && isset($_POST['password']) && isset($_POST['prenom'])  && isset($_POST['nom']) && isset($_POST['question']) && isset($_POST['reponse'])) {
-        $id = $_POST['id'];
-        $username = htmlspecialchars($_POST['username']);
-        $password = htmlspecialchars($_POST['password']);
-        $prenom = htmlspecialchars($_POST['prenom']);
-        $nom = htmlspecialchars($_POST['nom']);
-        $question = htmlspecialchars($_POST['question']);
-        $reponse = htmlspecialchars($_POST['reponse']);
-        $req = $bdd->prepare('SELECT password FROM user WHERE id = :id');
-        $req->execute(array(
-            'id' => $id
-        ));
-        $resultat = $req->fetch();
-        if ($password == $resultat['password']) {
-            $password_hash = $password;
-        } else {
-            $password_hash = password_hash($password, PASSWORD_DEFAULT);
+  else {
+	$id_user = $_SESSION['id'];
+	// Se connecter à la bdd pour récupérer les infos utilisateur
+    require "templates/database.php";
+	$req = $bdd->prepare('SELECT id, username, nom, prenom, password, question, reponse FROM user WHERE id = :id');
+	$req->execute(array(
+	    'id' => $id_user));
+	$resultat = $req->fetch();
+}
+if (isset($_POST['id']) && isset($_POST['username'])  && isset($_POST['prenom'])  && isset($_POST['nom']) && isset($_POST['password']) && isset($_POST['question']) && isset($_POST['reponse'])) {
+    $id = $_POST['id'];
+    $username = htmlspecialchars($_POST['username']);
+    $prenom = htmlspecialchars($_POST['prenom']);
+    $nom = htmlspecialchars($_POST['nom']);
+    $password = htmlspecialchars($_POST['password']);
+    $question = htmlspecialchars($_POST['question']);
+    $reponse = htmlspecialchars($_POST['reponse']);
+    $req = $bdd->prepare('SELECT password FROM user WHERE id = :id');
+    $req->execute(array(
+        'id' => $id_user
+    ));
+    $resultat = $req->fetch();
+    if ($password == $resultat['password']) {
+        $password_hash = $password;
+          } else {
+        $password_hash = password_hash($password, PASSWORD_DEFAULT);
         }
-        $req = $bdd->prepare('UPDATE user SET username = :username, password = :password, prenom = :prenom, nom = :nom, question = :question, reponse = :reponse WHERE id = :id');
-        $req->execute(array(
-            'id' => $id,
-            'username' => $username,
-            'password' => $password_hash,
-            'prenom' => $prenom,
-            'nom' => $nom,
-            'question' => $question,
-            'reponse' => $reponse
-        ));
-        $req->closeCursor();
     }
-
   if(isset($_POST['username'],  $_POST['prenom'], $_POST['nom'], $_POST['password'], $_POST['question'], $_POST['reponse']))
   {
       if(empty($_POST['password'])){
@@ -59,44 +53,53 @@
           echo "Veuillez répondre à la question secrète";
       }
       else{
-          $req = $bdd->prepare("INSERT INTO user(username, prenom, nom, password, question, reponse) VALUES(:username, :prenom, :nom, :password, :question, :reponse)");
-          $req->execute(array(
-              'username' => htmlspecialchars($_POST['username']),
-              'password' => password_hash($_POST['password'], PASSWORD_DEFAULT),
-              'prenom' => htmlspecialchars($_POST['prenom']),
-              'nom' => htmlspecialchars($_POST['nom']),
-              'question' => htmlspecialchars($_POST['question']),
-              'reponse' => htmlspecialchars($_POST['reponse'] )));
-          }
-  }
+        $req = $bdd->prepare('UPDATE user SET username = :username,  prenom = :prenom, nom = :nom, password = :password, question = :question, reponse = :reponse WHERE id = :id');
+        $req->execute(array(
+            'id' => $id_user,
+            'username' => $username,
+            'prenom' => $prenom,
+            'nom' => $nom,
+            'password' => $password_hash,
+            'question' => $question,
+            'reponse' => $reponse
+        ));
+        $req->closeCursor();
+    }
+}
+    session_start();
+    $_SESSION['id'] = $username;
+	$_SESSION['prenom'] = $prenom;
+	$_SESSION['nom'] = $nom;
 
         require "templates/head.php";
         require "templates/header.php";
-
     ?>
-        <main>
+        <div>
             <form class="formConnexion parametres" action="parametres.php" method="post">
-                <h1 class="white">Mon compte</h1>
-                <a href="index.php">Retour</a>
+                <h1 class="black compte">Mon compte</h1>
+                <a href="index.php" class="white">Retour</a>
+                <div class="champs">
+                    <input type="hidden" name="id_user" value="<?php echo $resultat['id_user']; ?>">
+                </div>
                 <div class="champs">
                     <label for="username"><b>Nom d'utilisateur</b><br /></label>
-                    <br /><h3><?php echo $_SESSION['username'] . ' '; ?></h3><br />
+                    <br /><h3 class="black"><?php echo $resultat['username'] . ' '; ?></h3>
                 </div>
                 <div class="champs">
                     <label for="prenom"><b>Prénom</b><br /></label>
-                    <br /><h3><?php echo 'prenom' . ' ' ?></h3><br />
+                    <input type="text" value='<?php echo $resultat['prenom'] ?>' name="prenom" required><br />
                 </div>
                 <div class="champs">
-                    <label for="prenom"><b>Prénom</b><br /></label>
-                    <br /><h3><?php echo $resultat['nom'] . ' ' ?></h3><br />
+                    <label for="prenom"><b>Nom de famille</b><br /></label>
+                    <input type="text" value='<?php echo $resultat['nom'] ?>' name="nom" required><br />
                 </div>
                 <div class="champs">
                     <label for="password"><b>Mot de passe</b></label><br />
-                    <input type="password" placeholder="Nouveau mot de passe..." name="password" id="password" required><br/>
+                    <input type="password" value='<?php echo $resultat['password'] ?>' name="password" id="password" required><br/>
                 </div>
                 <div class="champs">
                     <label for="confirmPassword"><b>Confirmer le mot de passe</b></label><br />
-                    <input type="password" placeholder="Confirmez le mot de passe..." name="confirmPassword" required><br />
+                    <input type="password" value='<?php echo $resultat['password'] ?>' name="confirmPassword" required><br />
                 </div>
                 <div class="champs">
                     <label for="question"><b>Question secrète</b><br /></label>
@@ -104,9 +107,9 @@
                 </div>
                 <div class="champs">
                     <label for="reponse"><b>Réponse</b><br /></label>
-                    <input type="text" placeholder="Réponse à la question secrète..." name="reponse" required><br />
+                    <input type="password" value='<?php echo $resultat['reponse'] ?>' name="reponse" required><br />
                     <br /><input type="submit" id='submit' value='Modifier mon compte' ><br />
                 </div>
             </form>
-        </main>
+        </div>
     <?php require "templates/footer.php"; ?>
