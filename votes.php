0c690d48e5ee29d2a7269e223e049629dd6ca19f
Clergé <65242993+Lokda-spec@users.noreply.github.com>
2021-06-16 21:03:30 +0200

    Update votes.php

--- votes.php
+++ votes.php
@@ -8,46 +8,33 @@
     die();
   }
   require "templates/database.php";
-
-  if(isset($_GET['votes']) && isset($_GET['id'])){
-		  $id_partenaires = $_GET['id'];
-		  $votes = htmlspecialchars($_GET['votes']);
-		  $id_user = $_SESSION['id'];
-		  $verif = $bdd -> prepare("SELECT * FROM partenaires WHERE id =?"); // vérifier que l'id partenaire est valable
-		  $verif -> execute(array($id_partenaires));
-		  $partenaires = $verif->fetch();
-			$verif->closeCursor();
-		  if($verif -> rowCount() == 1) // L'id partenaire est OK ==1
-		  {
-			if (($votes == "up" || $votes == "down") && !empty($partenaires)) {			  {
-				$votes = $bdd->prepare("SELECT * FROM votes WHERE id_partenaires = :id_partenaires AND id_user = :id_user");
-				$votes -> execute(array(
-					'id_partenaires' => $id_partenaires, //vérifier si l'user a déjà voté pour ce partenaire
-					'id_user' => $id_user ));
-				$result = $req->fetch();
-				$req->closeCursor();
-				if (empty($result)) { //result OK, il n'a pas voté pour cet acteur
-					$new_vote = $bdd->prepare('INSERT INTO votes(id_user, id_partenaires, votes) VALUES (:id_user, :id_partenaires, :votes)');
-					$new_vote->execute(array(
-						'id_user' => $id_user,
-						'id_partenaires' => $id_partenaires,
-						'votes' => $votes
-					));
-				} else {
-					if ($resultat['votes'] == $votes) //a déjà voté pour ce partenaire -- update ou redirection
-						$votes = 'imp';
-					$like = $bdd->prepare('UPDATE votes SET votes = :votes WHERE id_user = :id_user AND id_partenaires = :id_partenaires');
-					$like->execute(array(
-						'votes' => $votes,
-						'id_user' => $id_user,
-						'id_partenaires' => $id_partenaires
-					));
-				}
-				header('Location: partenaires.php?id=' . $id_partenaires);
-				die();}
-			}
+  //user ne peut voter qu'une seule fois
+  $req = $bdd->prepare('SELECT * FROM votes WHERE id_user = :id_user AND id_partenaires = :id_partenaires');
+		$req->execute(array(
+			'id_user' => $_SESSION['id'],
+			'id_partenaires' => $_GET['id']
+		));
+		$resultat = $req->fetch();
+		$req->closeCursor();
+		if (empty($resultat)) {
+			//si user n\'a pas encore voté, prendre en compte son vote dans la bdd
+			$req = $bdd->prepare('INSERT INTO votes(id, id_user, id_partenaires, votes) VALUES(:id, :id_user, :id_partenaires, :votes)');
+			$req->execute(array(
+				'id' => $_POST[''],
+				'id_user' => $_SESSION['id'],
+				'id_partenaires' => $_GET['id'],
+				'votes' => $_GET['votes'],
+				));
+		}else {
+			// si user a déjà voté, mise à jour de son vote
+			if (!empty ($resultat))
+				$votes = 'imp';
+			$up_vote = $bdd->prepare('UPDATE votes SET votes = :votes WHERE id_user = :id_user AND id_partenaires = :id_partenaires');
+			$up_vote->execute(array(
+				'votes' => $_GET['votes'],
+				'id_user' => $_SESSION['id'],
+				'id_partenaires' => $_GET['id'],
+			));
 		}
-	}
-
-header('Location: partenaires.php?id=' . $id_partenaires);
-?>
+  header('Location: partenaires.php?id=3');
+	?>
